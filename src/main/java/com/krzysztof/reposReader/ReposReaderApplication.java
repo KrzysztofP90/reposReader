081a95f2058b1588be062a4a57216f5e122c5416
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-08-02 11:47:25 +0200

    Initial commit

---
+++ src/main/java/com/krzysztof/reposReader/ReposReaderApplication.java
@@ -0,0 +1,13 @@
+package com.krzysztof.reposReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposReaderApplication.class, args);
+	}
+
+}
