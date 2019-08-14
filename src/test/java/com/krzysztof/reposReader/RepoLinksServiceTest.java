457b8a406d05958932a598cc83fc44ac2dd435d2
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-08-14 12:48:00 +0200

    Add test to repoLinksService - check correct of link to github repo with owner.

--- src/test/java/com/krzysztof/reposReader/RepoLinksServiceTest.java
+++ src/test/java/com/krzysztof/reposReader/RepoLinksServiceTest.java
@@ -1,8 +1,6 @@
 package com.krzysztof.reposReader;
 
 import com.krzysztof.reposReader.service.ReposLinksService;
-
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @ExtendWith(SpringExtension.class)
@@ -24,7 +22,6 @@ public class RepoLinksServiceTest {
         ReposLinksService service = new ReposLinksService();
         this.actualList = service.getAllReposLinksForUser("KrzysztofP90");
     }
-
 
     @Test
     public void testIfFirstMiddleAndLastRepoLinksStartFromHttps() {
@@ -40,5 +37,18 @@ public class RepoLinksServiceTest {
         assertEquals(actualMiddleHttps, expectedHttps);
         assertEquals(actualLastHttps, expectedHttps);
     }
-    
+
+    @Test
+    public void checkIfFirstMiddleAndLastRepoLinksContainCorrectGitHubAddressWithOwner() {
+        String owner = "KrzysztofP90";
+        int firstIndex = 0;
+        int middleIndex = actualList.size() / 2;
+        int lastIndex = actualList.size() - 1;
+        String githubAddressWithOwner = "https://github.com/" + owner;
+
+        assertTrue(actualList.get(firstIndex).contains(githubAddressWithOwner));
+        assertTrue(actualList.get(middleIndex).contains(githubAddressWithOwner));
+        assertTrue(actualList.get(lastIndex).contains(githubAddressWithOwner));
+    }
+
 }
